a245f48de677fef711b8bfb87997cf6dcef2d5c3
Samuel Riveros <sam_riveros18@outlook.com>
2022-03-31 10:20:07 -0400

    I adjusted the fortmat of the forcast cards. They now display the weather information based on coordinates

--- develop/js/script.js
+++ develop/js/script.js
@@ -1,3 +1,4 @@
+
 var userSearch = [];
 var APIKey= 'a12c8ed33df1450f8eb9318841d28b8e';
 var weatherApiUrl= 'https://api.openweathermap.org';
@@ -7,10 +8,9 @@ var city = document.querySelector('#userSearch-city');
 
 
 fetchWeather = (location) => {
-    var lat = 33.753746;
-    var lon = -84.386330;
+    var  lat  = 33.753746;
+    var  lon  =  -84.386330;
     var units = 'imperial'
-    
     var apiURL = `${weatherApiUrl}/data/2.5/onecall?lat=${lat}&lon=${lon}&units=${units}&exclude=minutely,hourly&appid=${APIKey}`
 
     fetch(apiURL)
@@ -19,8 +19,7 @@ fetchWeather = (location) => {
             return res.json();
         })
         .then((data) => {
-            
-            console.log(data);
+            showWeather(data);
         })
         .catch((err)=>{
             console.error(err);
@@ -33,7 +32,7 @@ renderHistory = (city,data)=> {
 }
 
 fetchCoordinates = (search) =>{
-    var apiUrl = `${eatherApiUrl}/geo/1.0/direct?q=${search}&limit=5&appid=${ApiKey}`;
+    var apiUrl = `${weatherApiUrl}/geo/1.0/direct?q=${search}&limit=5&appid=${ApiKey}`;
 
     fetch(apiUrl)
         .then((res) => {
@@ -44,7 +43,7 @@ fetchCoordinates = (search) =>{
                 alert('Location not found')
             }
             else{
-                
+                appendToHistory(search);
                 fetchWeather(data[0]);
             }
         })
@@ -53,3 +52,26 @@ fetchCoordinates = (search) =>{
         })
 }
 
+
+
+showWeather = (response)=>{
+    console.log(response)
+
+    var row = document.querySelector('.weather.row');
+    
+    row.innerHTML = response.daily.map(day =>{
+        var dateTime = new Date(day.dt * 1000);//change this format
+        return `<div class="card" style="width: 10rem">
+        <h5 class="card-title p-2">${dateTime.toString()}</h5>
+        <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@4x.png" class="card-img-top" alt="${day.weather[0].description}">
+        <div class="card-body">
+            <h3 class="card-title">${day.weather[0].main}</h3>
+            <p class="card-text">Temp: ${day.temp.day}°F</p>
+            <p class="card-text">Humidity: ${day.humidity}%</p>
+            <p class="card-text">Wind: ${day.wind_speed}mph</p>
+        </div>
+    </div>`
+    }).join('');
+}
+
+fetchWeather();
